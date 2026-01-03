6287683c4ce9e9e76ffc76a8be1def9f2b719c91
chammodesu-na <chammodesu@gmail.com>
2026-01-04 01:57:10 +0900

    Update layer_status.c

--- boards/shields/dongle_display/widgets/layer_status.c
+++ boards/shields/dongle_display/widgets/layer_status.c
@@ -60,7 +60,7 @@ ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
 int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_t *parent) {
     widget->obj = lv_label_create(parent);
     lv_obj_set_width(widget->obj, CONFIG_ZMK_DONGLE_DISPLAY_LAYER_NAME_SCROLL_WIDTH);
-    lv_label_set_long_mode(widget->obj, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_label_set_long_mode(widget->obj, LV_LABEL_LONG_CLIP);
     lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_12,
                            LV_PART_MAIN | LV_STATE_DEFAULT);
 
