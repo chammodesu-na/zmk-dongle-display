a076358916979dbeb44f585159109dc9e8a65641
chammodesu-na <chammodesu@gmail.com>
2026-01-17 00:43:27 +0900

    Update output_status_sym.c

--- boards/shields/dongle_display/widgets/output_status_sym.c
+++ boards/shields/dongle_display/widgets/output_status_sym.c
@@ -3,9 +3,8 @@
  *
  * SPDX-License-Identifier: MIT
  */
- 
- #include <lvgl.h>
 
+#include <lvgl.h>
 
 #ifndef LV_ATTRIBUTE_MEM_ALIGN
 #define LV_ATTRIBUTE_MEM_ALIGN
@@ -23,7 +22,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_1 uin
 };
 
 const lv_img_dsc_t sym_1 = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨] LV_COLOR_FORMAT_I1 -> LV_IMG_CF_INDEXED_1BIT
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -42,7 +41,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_2 uin
 };
 
 const lv_img_dsc_t sym_2 = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -61,7 +60,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_3 uin
 };
 
 const lv_img_dsc_t sym_3 = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -80,7 +79,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_4 uin
 };
 
 const lv_img_dsc_t sym_4 = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -99,7 +98,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_5 uin
 };
 
 const lv_img_dsc_t sym_5 = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 5,
   .header.h = 6,
   .data_size = 14,
@@ -118,7 +117,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_OK ui
 };
 
 const lv_img_dsc_t sym_ok = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 5,
   .header.h = 5,
   .data_size = 13,
@@ -137,7 +136,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_NOK u
 };
 
 const lv_img_dsc_t sym_nok = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 5,
   .header.h = 5,
   .data_size = 13,
@@ -156,7 +155,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_OPEN 
 };
 
 const lv_img_dsc_t sym_open = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 5,
   .header.h = 5,
   .data_size = 13,
@@ -178,7 +177,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_BT ui
 };
 
 const lv_img_dsc_t sym_bt = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 9,
   .header.h = 14,
   .data_size = 36,
@@ -200,12 +199,9 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SYM_USB u
 };
 
 const lv_img_dsc_t sym_usb = {
-  .header.cf = LV_COLOR_FORMAT_I1,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT, // [수정됨]
   .header.w = 9,
   .header.h = 14,
   .data_size = 36,
   .data = sym_usb_map,
 };
-
-
-
